b4bdc458f597777a5937ec9a309ef1e9406ca470
Agustin <agustin.garcia@zubi.group>
2023-09-11 13:50:13 +0200

    actualizado8

---
+++ dashboard/dash.js
@@ -0,0 +1,35 @@
+
+// var sidebarOpen = false;
+// var sidebar = document.getElementById("sidebar");
+
+// function openSidebar(){
+//     if(!sidebarOpen){
+//         sidebar.classList.add=("sidebar-responsive");
+//         sidebarOpen = true;
+//     }
+// }
+
+// function closeSidebar() {
+//     if(sidebarOpen) {
+//         sidebar.classList.remove("sidebar-responsive");
+//         sidebarOpen = false;
+//     }
+// }
+
+
+
+
+
+
+// var sidebarOpen = false;
+// var sidebar = document.getElementById("sidebar");
+
+// function toggleSidebar() {
+//     if (sidebarOpen) {
+//         sidebar.classList.remove("sidebar-responsive");
+//         sidebarOpen = false;
+//     } else {
+//         sidebar.classList.add("sidebar-responsive");
+//         sidebarOpen = true;
+//     }
+// }
